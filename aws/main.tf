32e94ab2f984a784ea9f167a13df10f36ee1b708
sandeepmchary <awssandeepchary@gmail.com>
2021-12-15 16:07:51 +0530

    added files

---
+++ aws/main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  profile = "terra"
+  region = "us-east-1"
+
+}
+resource "aws_instance" "ragh_terr" {
+  ami = "ami-04505e74c0741db8d"
+  instance_type = 
+  tags = {
+    "Name" = "ragh_terra"
+  }
+}
+
+variable "instance_type" {
+  description = "AMI for the instance"
+  type = string
+  default = "t2.micro"
+}
