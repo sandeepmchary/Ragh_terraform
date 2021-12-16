3fd9191dfcae85caf74ad9565448dde58d0ef401
sandeepmchary <awssandeepchary@gmail.com>
2021-12-16 17:16:30 +0530

    added tfvars file

--- aws_varibles/variable.tf
+++ aws_varibles/variable.tf
@@ -1,6 +1,10 @@
+# here we are trying with terraform.tfvars this code was used before
+# for terraform.tfvars to work we give empty files in the variables folder
+# values for this variables are given in the terraform.tfvars file
+
 variable "instance_type" {
-  description = "EC2 instance type"
+  /*description = "EC2 instance type"
   type = string
-  default = "t2.micro"
+  default = "t2.micro"*/
 
 }
